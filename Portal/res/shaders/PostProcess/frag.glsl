f24adf7bf7170dcc43db3051a1bdf75b7b259750
JPaja <jovan.pavlovic99@gmail.com>
2021-01-18 17:19:04 +0100

    fix

--- Portal/res/shaders/PostProcess/frag.glsl
+++ Portal/res/shaders/PostProcess/frag.glsl
@@ -8,6 +8,6 @@ uniform sampler2D albedo;
 
 void main()
 {
-	vec3 colour = texture(albedo,pass_uv).rgb;
+ 	vec3 colour = texture(albedo,pass_uv).rgb;
 	final_colour = vec4(1 - colour.r,1 - colour.g,1 - colour.b,1.0);
 }
